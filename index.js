870be9a55986dd619c62258322ecaadd590787c1
Mateo Ivanovich <mateoivanovich43@gmail.com>
2022-11-27 15:25:44 -0300

    Primera PreEntrega 1 finalizada

--- index.js
+++ index.js
@@ -1,36 +1,52 @@
 
 alert("ESTA ES UNA PAGINA DE COTIZACIONES DE VEHICULOS, USTED PODRA ASEGURAR LA CANTIDAD DE VEHICULOS QUE QUIERA, SOLO ASEGURAMOS AUTOS, MOTOS O BICIS")
 
-let cantidadDeVehiculos = prompt("Cuantos vehiculos queres asegurar?")
+const pagoMensual = () => {
+    let cantidadDeVehiculos = prompt("Cuantos vehiculos queres asegurar?")
+    let seguroBici = 200
+    let seguroAuto = 1000
+    let seguroMoto = 700
+    let valorSeguros = 0
 
-let seguroBici = 200
-let seguroAuto = 1000
-let seguroMoto = 700
+    for( let i = 0; i < cantidadDeVehiculos; i++){
 
-let seAsocio = false
+        let vehiculo = prompt(`Tu vehiculo ${i+1} es un auto, una bici o una moto?`)
+        vehiculo.toLowerCase()
 
-let valorSeguros = 0
+        if(vehiculo == "bici"){
+            valorSeguros = valorSeguros + seguroBici
+        }else if(vehiculo == "moto"){
+            valorSeguros = valorSeguros + seguroMoto
+        }else if(vehiculo == "auto"){
+            valorSeguros = valorSeguros + seguroAuto
+        }
+    }
 
-for( let i = 0; i < cantidadDeVehiculos; i++){
+    return valorSeguros
+}
 
-    let vehiculo = prompt(`Tu vehiculo ${i+1} es un auto, una bici o una moto?`)
+const asociarse = () => {
+    let precio = pagoMensual()
+    let preguntaAsociarse = prompt(`El valor total que debes abonar mensualmente es de ${precio} pesos\nDesea asociarse con nuestra empresa de seguros?\n(si o no)`)
+    preguntaAsociarse.toLocaleLowerCase()
 
-    if(vehiculo == "bici"){
-        valorSeguros = valorSeguros + seguroBici
-    }else if(vehiculo == "moto"){
-        valorSeguros = valorSeguros + seguroMoto
-    }else if(vehiculo == "auto"){
-        valorSeguros = valorSeguros + seguroAuto
+    let seAsocio = false
+
+    if(preguntaAsociarse == "si"){
+        seAsocio = true
+    }
+
+    return seAsocio
+}
+
+const cotizadorDeSeguros = () => {
+    let seAsocio = asociarse()
+
+    if(seAsocio == true){
+        alert("Gracias por asociarte, a continuacion usted podra confirmar los metodos de pago en la pagina web.")
+    }else{
+        alert("Que tenga un buen dia!")
     }
 }
 
-let asociarse = prompt(`El valor total que debes abonar mensualmente es de ${valorSeguros} pesos\nDesea asociarse con nuestra empresa de seguros?\n(si o no)`)
-
-if(asociarse == "si"){
-    seAsocio = true
-}
-
-//Lo hago para la utilizacion de Booleanos, ya se que no hace falta jajaj
-if(seAsocio == true){
-    alert("Gracias por asociarte, a continuacion usted podra confirmar los metodos de pago en la pagina web.")
-}
+console.log(cotizadorDeSeguros())
